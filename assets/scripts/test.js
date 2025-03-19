3ec3014605563d705c40fbdc48ad3c4e1bb86efd
User <rkw2115@gmail.com>
2025-03-19 23:17:50 +0900

    내위치 화장실위치 마커표시

--- assets/scripts/test.js
+++ assets/scripts/test.js
@@ -1,100 +1,84 @@
-
-
-
 const $map = document.getElementById('map');
 const $toyBtn = $map.querySelector(':scope > .loc-to > .toilet > .button');
 const $nowBtn = $map.querySelector(':scope > .loc-to > .location > .button');
+
 const mapInstance = new kakao.maps.Map($map, {
-    center: new kakao.maps.LatLng(35.8655753, 128.59339),
+   center: new kakao.maps.LatLng(35.8655753, 128.59339),
     level: 3,
 });
 
-let lat;
-let lon;
-let nowPosition;
-let nowMarker;
+let currentLat;
+let currentLon;
+let currentPosition;
+let currentMarker;
 
 
-
-const nowInfoWindow = new kakao.maps.InfoWindow ({
-    content: '<div style="width: 3rem; padding: 0.5rem 0; text-align: center;">현재위치</div>',
-});
-const toyInfoWindow = new kakao.maps.InfoWindow ({
-    content: '<div style="width: 3rem; padding: 0.5rem 0; text-align: center;">화장실</div>',
+const currentInfoWindow = new kakao.maps.InfoWindow({
+    content: '<div style="width: 5rem; padding: 0.5rem 0; text-align: center;">현재위치</div>',
 });
 
+const toiletInfoWindow = new kakao.maps.InfoWindow({
+    content: '<div style="width: 5rem; padding: 0.5rem 0; text-align: center;">화장실</div>',
+});
 
+let toiletMarkers = [];
 
-const now = () => {
+const findCurrentLocation = () => {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
-            lat = position.coords.latitude,
-                lon = position.coords.longitude;
+           currentLat = position.coords.latitude;
+           currentLon = position.coords.longitude;
 
-            nowPosition = new kakao.maps.LatLng(lat, lon);
-            nowMarker?.setMap(null);
-            nowMarker = new kakao.maps.Marker({
-                map: mapInstance,
-                position: nowPosition,
-            });
-            nowInfoWindow.open(mapInstance, nowMarker);
-            mapInstance.setCenter(nowPosition);
-            mapInstance.setLevel(3);
+           currentPosition = new kakao.maps.LatLng(currentLat, currentLon);
 
+           if (currentMarker) currentMarker.setMap(null);
+           currentMarker = new kakao.maps.Marker({
+              map: mapInstance,
+              position: currentPosition,
+           });
+           currentInfoWindow.open(mapInstance, currentMarker);
+
+           mapInstance.setCenter(currentPosition);
+           mapInstance.setLevel(3);
         });
+    } else {
+        alert('위치 정보를 사용할 수 없습니다.');
     }
 };
 
-let toPosLat;
-let toPosLon;
-let toyPosition;
-let toyMarker;
+const findToilets = () => {
 
-let toyMarkers = [];
-const findToilet = () => {
-    for (const list of lists) {
-        toPosLat = [list[5]];
-        toPosLon = [list[6]];
-        toyPosition = new kakao.maps.LatLng(toPosLat, toPosLon);
-        toyMarker?.setMap(null);
-        toyMarker = new kakao.maps.Marker({
-           map: mapInstance,
-           position: toyPosition
-        });
-        toyInfoWindow.open(mapInstance, toyMarker);
-        mapInstance.setCenter(toyPosition);
-        mapInstance.setLevel(3);
-    }
+  toiletMarkers.forEach(marker => marker.setMap(null));
+  toiletMarkers = [];
+
+  const filterToilets = toilets.filter(data => data['C0'] !== "번호");
+
+  for (const toilet of toilets) {
+      const lat = parseFloat(toilet['C20']);
+      const lng = parseFloat(toilet['C21']);
+
+      if (isNaN(lat) || isNaN(lng)) continue;
+      const position = new kakao.maps.LatLng(lat, lng);
+      const marker = new kakao.maps.Marker({
+         map: mapInstance,
+         position: position
+      });
+
+      const infoContent = `
+            <div style="width: 12rem; padding: 0.5rem; text-align: center;">
+                <strong>${toilet['C3']}</strong><br>
+                <small>${toilet['C4']}</small><br>
+                <small>개방시간: ${toilet['C17']} ${toilet['C18'] || ''}</small>
+            </div>
+        `;
+
+      const infoWindow = new kakao.maps.InfoWindow({
+         content: infoContent
+      });
+      kakao.maps.event.addListener(marker, 'click', function() {
+         infoWindow.open(mapInstance, marker);
+      });
+      toiletMarkers.push(marker);
+  }
+
 };
-
-
-
-$nowBtn.onclick = () => {
-    now();
-}
-
-$toyBtn.onclick = () => {
-    findToilet();
-}
-
-const dates = JSON.parse(JSON.stringify(toilets));
-
-//c3, c4, c5, c17... lists 빈 배열 만들어서 쑤셔넣고 위도 경도 빼와서 toiletPosition 변수 만들고 카카오맵위도경도에 쑤셔넣고 마커띄운다
-
-lists = [];
-for (const data of dates) {
-    tod = [data['C3'],
-    data['C4'],
-    data['C5'],
-    data['C17'],
-        data['C18'],
-        data['C20'],
-        data['C21']
-    ]
-    lists.push(tod);
-}
-
-toPos = [];
-
-console.log(lists);
-//toPos가 위도 경도 담긴 배열
