53f2278e3716ddac941a8acd4626ea7e8cbfb94e
User <rkw2115@gmail.com>
2025-03-30 22:05:46 +0900

    gitignore file add

---
+++ assets/scripts/config.js
@@ -0,0 +1,3 @@
+const CONFIG = {
+    KAKAO_APP_KEY: "0d4d42547bb98aba4a01379af27de88d"
+};
--- assets/scripts/test.js
+++ assets/scripts/test.js
@@ -1,70 +1,9 @@
-const $map = document.getElementById('map');
-const $toyBtn = $map.querySelector(':scope > .loc-to > .toilet > .button');
-const $nowBtn = $map.querySelector(':scope > .loc-to > .location > .button');
+const $map = document.getElementById('map-container');
+const $mapElement = document.getElementById('map');
+const $toyBtn = $map.querySelector(':scope > .control-buttons > .control-button.toilet-finder > .icon-button');
+const $nowBtn = $map.querySelector(':scope > .control-buttons > .control-button.location-finder > .icon-button');
+const $infoPanel = $map.querySelector(':scope > .info-panel');
 
-const mapInstance = new kakao.maps.Map($map, {
-   center: new kakao.maps.LatLng(35.8655753, 128.59339),
-    level: 3,
+const mapInstance = new kakao.maps.Map($mapElement, {
+   center: new kakao.maps.LatLng(35.8655753, 128.59339)
 });
-
-let currentLat;
-let currentLon;
-let currentPosition;
-let currentMarker;
-
-
-const currentInfoWindow = new kakao.maps.InfoWindow({
-    content: '<div style="width: 5rem; padding: 0.5rem 0; text-align: center;">현재위치</div>',
-});
-
-const toiletInfoWindow = new kakao.maps.InfoWindow({
-    content: '<div style="width: 5rem; padding: 0.5rem 0; text-align: center;">화장실</div>',
-});
-
-let toiletMarkers = [];
-
-const findCurrentLocation = () => {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-           currentLat = position.coords.latitude;
-           currentLon = position.coords.longitude;
-
-           currentPosition = new kakao.maps.LatLng(currentLat, currentLon);
-
-           if (currentMarker) currentMarker.setMap(null);
-           currentMarker = new kakao.maps.Marker({
-              map: mapInstance,
-              position: currentPosition,
-           });
-           currentInfoWindow.open(mapInstance, currentMarker);
-
-           mapInstance.setCenter(currentPosition);
-           mapInstance.setLevel(3);
-        });
-    } else {
-        alert('위치 정보를 사용할 수 없습니다.');
-    }
-};
-
-const findToilets = () => {
-
-  toiletMarkers.forEach(marker => marker.setMap(null));
-  toiletMarkers = [];
-
-  const filterToilets = toilets.filter(data => data['C0'] !== "번호");
-
-  for (const toilet of toilets) {
-      const lat = parseFloat(toilet['C20']);
-      const lng = parseFloat(toilet['C21']);
-
-      if (isNaN(lat) || isNaN(lng)) continue;
-      const position = new kakao.maps.LatLng(lat, lng);
-      const marker = new kakao.maps.Marker({
-         map: mapInstance,
-         position: position
-      });
-
-      toiletMarkers.push(marker);
-  }
-
-};
